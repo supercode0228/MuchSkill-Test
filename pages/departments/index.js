d119cb4029f30e9747c4c74a0ffe6a40039e6ce3
supercode0228 <bee0228angel@outlook.com>
2020-08-27 11:42:26 -0400

    Integrated fetch department api call with graphql query.

--- pages/departments/index.js
+++ pages/departments/index.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import { ApolloConsumer, Query } from 'react-apollo'
+import { isEmpty } from 'lodash'
 import { Button } from 'reactstrap'
+
+import { FETCH_DEPARTMENTS } from '../../gqls'
+
+import { DepartmentsDataTable } from '../../components/Departments'
 
 import styles from '../../styles/Department.module.css'
 
 const Departments = () => {
+  let departments = []
+
   return (
     <div className="container">
       <div className={styles.department__header}>
         <h3>Departments</h3>
         <Button className="button btn-black">Add</Button>
       </div>
+      <ApolloConsumer>
+        {(client) => (
+          <Query query={FETCH_DEPARTMENTS}>
+            {({ data, loading, error }) => {
+              if (error) return false
+              if (isEmpty(data)) return false
+              departments = data.fetch_departments
+              return (
+                <DepartmentsDataTable
+                  data={departments}
+                  onDeleteDlg={handleDeleteDlgToggle}
+                  onEditDlg={handleDlgToggle}
+                />
+              )
+            }}
+          </Query>
+        )}
+      </ApolloConsumer>
     </div>
   )
 }
