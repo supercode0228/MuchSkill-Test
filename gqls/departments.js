768778b4007db64288792b8ae51069313e70d898
supercode0228 <bee0228angel@outlook.com>
2020-08-27 12:05:07 -0400

    Defined the gql for edit department on client side.

--- gqls/departments.js
+++ gqls/departments.js
@@ -8,9 +8,19 @@ export const FETCH_DEPARTMENTS = gql`
     }
   }
 `
+
 export const ADD_DEPARTMENT = gql`
   mutation add_department($name: String!) {
     add_department(name: $name) {
+      _id
+      name
+    }
+  }
+`
+
+export const EDIT_DEPARTMENT = gql`
+  mutation edit_department($name: String!, $_id: ID!) {
+    edit_department(name: $name, _id: $_id) {
       _id
       name
     }
