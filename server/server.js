34866fc22d1529b434170230067d0eab8c1e9091
supercode0228 <bee0228angel@outlook.com>
2020-08-27 11:56:12 -0400

    Integrated Apollo server graphql in express.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,28 @@ require('dotenv').config({ path: __dirname + '/.env' })
 const express = require('express')
 const mongoose = require('mongoose')
 
+const { ApolloServer } = require('apollo-server-express')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+
 const dbURI = require('./config/db')
-const { port } = require('./config/config')
+const { port, clientURI } = require('./config/config')
 
 const app = express()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const corsOptions = {
+  origin: clientURI,
+  credentials: true,
+  optionsSuccessStatus: 200,
+}
+
+server.applyMiddleware({ app, cors: corsOptions, path: '/graphql' })
 
 mongoose
   .connect(dbURI, {
