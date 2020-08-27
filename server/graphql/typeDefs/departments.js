78f1a603cf1894aea4dedf3aea5ea2fac20a39ce
supercode0228 <bee0228angel@outlook.com>
2020-08-27 13:04:21 -0400

    Defined the typeDefs for add/edit department.

--- server/graphql/typeDefs/departments.js
+++ server/graphql/typeDefs/departments.js
@@ -5,6 +5,11 @@ module.exports = gql`
     fetch_departments: [Department]
   }
 
+  extend type Mutation {
+    add_department(name: String!): Department
+    edit_department(name: String!, _id: ID!): Department
+  }
+
   type Department {
     _id: ID!
     name: String!
