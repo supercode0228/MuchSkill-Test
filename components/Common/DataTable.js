7a49edb946e71274077c459e4872e9bdbfa30db7
supercode0228 <bee0228angel@outlook.com>
2020-08-27 11:38:54 -0400

    Built the Common reusable DataTable component.

---
+++ components/Common/DataTable.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Table, Button } from 'reactstrap'
+
+const DataTable = (props) => {
+  const { columns, data, actions } = props
+  return (
+    <Table hover responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            {Object.keys(item).map((key) => {
+              if (key !== '_id' && key !== '__typename')
+                return <th key={item[key]}>{item[key]}</th>
+            })}
+            <th>
+              {actions.map((action, index) => (
+                <Button
+                  key={index}
+                  className={action.classname}
+                  onClick={(e) => {
+                    action.function(item)
+                  }}
+                >
+                  {action.name}
+                </Button>
+              ))}
+            </th>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default DataTable
