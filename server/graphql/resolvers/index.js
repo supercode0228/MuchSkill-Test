51a8db507b110f842d759b5487bcb551afdf4b5b
supercode0228 <bee0228angel@outlook.com>
2020-08-27 13:43:24 -0400

    Defined the resolvers for fetch/add/edit/delete techtool on server side.

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const { merge } = require('lodash')
 
 const departments = require('./departments')
+const techtools = require('./techtools')
 
-module.exports = merge(departments)
+module.exports = merge(departments, techtools)
---
+++ server/graphql/resolvers/techtools.js
@@ -0,0 +1,57 @@
+const TechTool = require('../../models/TechTool')
+
+module.exports = {
+  Query: {
+    fetch_techtools: async () => {
+      try {
+        const techtools = await TechTool.find().exec()
+        if (Array.isArray(techtools) && techtools.length) {
+          return techtools
+        }
+      } catch (err) {
+        console.log(err)
+        return err
+      }
+    },
+  },
+  Mutation: {
+    add_department: async (args, req) => {
+      try {
+        const { name } = req
+        const techtool = await TechTool.create({ name })
+
+        if (techtool) {
+          return techtool
+        }
+      } catch (err) {
+        console.log(err)
+        return err
+      }
+    },
+    edit_techtool: async (args, req) => {
+      try {
+        const { _id, name } = req
+        const techtool = await TechTool.findByIdAndUpdate(
+          { _id },
+          { $set: { name: name } },
+          { new: true },
+        )
+
+        if (techtool) return techtool
+      } catch (err) {
+        console.log(err)
+        return err
+      }
+    },
+    delete_techtool: async (args, req) => {
+      try {
+        const _id = req._id
+        const techtool = await TechTool.findByIdAndDelete({ _id })
+        return techtool
+      } catch (err) {
+        console.log(err)
+        return err
+      }
+    },
+  },
+}
