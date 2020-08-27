e66d4a48002ae481c70feda758c9e2fb2aa52aff
supercode0228 <bee0228angel@outlook.com>
2020-08-27 13:20:47 -0400

    Developed the typeDef for delete department.

--- server/graphql/typeDefs/departments.js
+++ server/graphql/typeDefs/departments.js
@@ -8,6 +8,7 @@ module.exports = gql`
   extend type Mutation {
     add_department(name: String!): Department
     edit_department(name: String!, _id: ID!): Department
+    delete_department(_id: ID!): Department
   }
 
   type Department {
